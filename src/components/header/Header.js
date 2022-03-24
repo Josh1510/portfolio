f2cd81598ac19254200155ebb0f59073c73830bb
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-24 18:40:51 +1100

    smooth scroll down to sections

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,9 @@ import './Header.css';
 export default function Header() {
   return (
     <div className="portfolio__header">
-      <div className="header__link">Projects</div>
+      <div className="header__link">
+        <a href="/#projects">Projects</a>
+      </div>
       <div className="header__link">Say Hello</div>
     </div>
   );
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -3,9 +3,9 @@ import './Projects.css';
 
 export default function Projects() {
   return (
-    <div className="portfolio__projects">
+    <section id="projects" className="portfolio__projects">
       <h4 className="projects__header">Projects</h4>
       <div>Projects</div>
-    </div>
+    </section>
   );
 }
