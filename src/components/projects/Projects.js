4b994435227615fd04e2f0f17bcc2da613fadee9
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-24 18:34:01 +1100

    initial

---
+++ src/components/projects/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Projects.css';
+
+export default function Projects() {
+  return (
+    <div className="portfolio__projects">
+      <h4 className="projects__header">Projects</h4>
+      <div>Projects</div>
+    </div>
+  );
+}
