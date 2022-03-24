7e8871f3f8905bc7a3ece4ac0cc9044435bed05d
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-24 18:35:04 +1100

    content

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,7 +4,7 @@ import './About.css';
 export default function About() {
   return (
     <div className="portfolio__about">
-      <h1 className="about__small-heading">Hey, I'm</h1>
+      <h1 className="about__small-heading">Hey there, I'm</h1>
       <h2 className="about__large-heading">Josh Moon</h2>
       <h3 className="about__large-heading">I'm learning to build things for the web.</h3>
       <p>I'm passionate about learning.</p>
