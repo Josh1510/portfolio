7063b6830f38b72d8f40a5160238d14050a8c4f9
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-24 18:33:55 +1100

    projects component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import WordleApp from './projects/wordle/WordleApp';
 import Header from './components/header/Header';
 import About from './components/about/About';
+import Projects from './components/projects/Projects';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <header className="App-header">
         <Header />
         <About />
+        <Projects />
         {/* <WordleApp /> */}
       </header>
     </div>
