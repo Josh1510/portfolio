d773b4a5ff92d7c2610e0e5467aa66c8ce024304
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-24 17:33:51 +1100

    content

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,6 +7,11 @@ export default function About() {
       <h1 className="about__small-heading">Hey, I'm</h1>
       <h2 className="about__large-heading">Josh Moon</h2>
       <h3 className="about__large-heading">I'm learning to build things for the web.</h3>
+      <p>I'm passionate about learning.</p>
+      <p>
+        I'm currently focused on front end development with react but have also built some projects using Node.js, Express, Cassandra DB,
+        and NoSQL.
+      </p>
     </div>
   );
 }
