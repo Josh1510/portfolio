9303d3a789b06584ef12248ebc7767139dc7492b
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-26 16:47:23 +1100

    formatting change - description next to image rather than over top

--- src/components/projects/ProjectBox.js
+++ src/components/projects/ProjectBox.js
@@ -11,11 +11,12 @@ export default function ProjectBox({ data }) {
   return (
     <div className="project__box-container">
       <h5>{data.title}</h5>
-
-      <div className="box__image-container">
+      <div className="project__details-container">
         <img src={images[data.image]} alt={`screenshot of ${data.title}`}></img>
         <div className="box__description-container">
-          <div className="description__data">{data.description}</div>
+          <div className="description__data-container">
+            <div className="description__data">{data.description}</div>
+          </div>
           <div className="description__button-container">
             <div className="description__button">DEMO</div>
           </div>
