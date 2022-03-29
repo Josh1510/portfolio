d7bb246319e1d75c56f220eeedb87a767ea577fc
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-29 14:14:37 +1100

    restaurant site data

--- src/projects/projectsData.js
+++ src/projects/projectsData.js
@@ -10,12 +10,22 @@ const projectData = [
   {
     title: 'Shop and cart',
     description:
-      'A shop front and cart, shopfront built with react and styled components, back end managed by Express with a PostgreSQL database.',
+      'A shop front and cart. The Shopfront built with react and styled components, back end managed by Express with a PostgreSQL database.',
     image: './Images/shopimg.png',
     video: './Videos/shop_video.mp4',
     demo: false,
-    github: 'https://github.com/Josh1510/shope',
+    github: 'https://github.com/Josh1510/shop',
     project: 'Shop',
+  },
+  {
+    title: 'Restaurant Site',
+    description:
+      'One page restaurant website. Built with React and React Router, login is handled by Firebase authentication. A venue can log into the backend to update their important information',
+    image: './Images/restaurantimg.png',
+    video: './Videos/restaurant_video.mp4',
+    demo: false,
+    github: 'https://github.com/Josh1510/restaurant-react',
+    project: 'Restaurant',
   },
 ];
 
