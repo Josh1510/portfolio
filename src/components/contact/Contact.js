3f95d3d4caef587115613766c763b3563b77ff29
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-30 16:54:13 +1100

    contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,19 +3,23 @@ import './Contact.css';
 
 export default function Contact() {
   return (
-    <section className="contact">
+    <section id="contact" className="contact">
       <h4 className="contact__header">Say Hello</h4>
       <div className="contact__container">
         <div className="contact__p-container">
           <p>
-            I'm currently looking for a junior or graduate role where I will be able to expand my knowledge around all things software
-            engineering - not just web!
+            I'm looking for a junior or graduate role where I will be able to expand my knowledge around all things software engineering -
+            not just web!
           </p>
           <p>If you think you might have a suitable role I'd love to talk, please send me an email or contact me through LinkedIn!</p>
         </div>
         <div className="contact__button-container">
-          <div className="contact__button-email">Email</div>
-          <div className="contact__button-linkedin">LinkedIn</div>
+          <div className="contact__button">
+            <a href="mailto:joshmoon1510+portfolio@gmail.com">Email</a>
+          </div>
+          <div className="contact__button">
+            <a href="https://www.linkedin.com/in/joshua-moon-770324106/">LinkedIn</a>
+          </div>
         </div>
       </div>
     </section>
